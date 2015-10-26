58fa0691ffbd89d74f0431899f0be2ce9c87676c
Steve Sun <jiahuan@amazon.com>
2015-10-25 20:41:07 -0700

    commit chapter 1 reinforcement exercise

---
+++ src/chapter1/reinforcement/Reinforcement.java
@@ -0,0 +1,11 @@
+package chapter1.reinforcement;
+
+public class Reinforcement {
+	/**
+	 * R1.1 Answer: After the clone, A[4] and B[4] are both pointing to the same
+	 * GameEntry object.
+	 */
+	
+	/**R1.2 
+	 * Too easy to do it. Pass.*/
+}
